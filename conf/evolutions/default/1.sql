8a4c620d190e3c059fc72cc7b43485835d147b98
Nikita Melkozerov <n.melkozerov@gmail.com>
2014-03-02 14:21:45 +0600

    added first database evolution

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- First database schema
+
+# --- !Ups
+
+set ignorecase true;
+
+create table number (
+  id                        bigint not null,
+  name                      varchar(255) not null,
+  phoneNumber               varchar(255) not null,
+  constraint pk_number primary key (id))
+;
+
+create sequence number_seq start with 1000;
+
+create index ix_num_name on number (name);
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists number;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists number_seq;
